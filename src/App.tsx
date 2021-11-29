383bf4bbf4e0afcb841fe7b8a2eb3c7360e896d0
Edgar Andrés Angrino Lafaux <mix7reload@gmail.com>
2021-11-28 21:14:36 -0500

    Se crean las rutas para cada pagina en especifico

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,22 @@ import { Fragment } from "react";
 import "./App.scss";
 import "./config";
 import Buttom from "./components/Buttom";
+import User from "./pages/User";
+import UserList from "./pages/UserList";
+import UserAdd from "./pages/UserAdd";
+import UserDelete from "./pages/UserDelete";
+import UserUpdate from "./pages/UserUpdate";
 import { Route, Routes } from "react-router";
 
 const App = () => (
   <Fragment>
     <Routes>
       <Route path="/" element={<Buttom />} />
+      <Route path="/getitem" element={<User />}/>
+      <Route path="/get" element={<UserList />}/>
+      <Route path="/insert" element={<UserAdd />}/>
+      <Route path="/delete" element={<UserDelete />}/>
+      <Route path="/update" element={<UserUpdate />}/>
     </Routes>
   </Fragment>
 );
